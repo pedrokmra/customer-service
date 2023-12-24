793f51cad66fe577bf50b2fc291b62f941fd485e
pedrok <pedrokimur@icloud.com>
2023-12-24 14:46:17 -0300

    refactor(customer): remove boilerplate

--- src/main/java/com/pedrok/customerservice/customer/CustomerControllerV2.java
+++ src/main/java/com/pedrok/customerservice/customer/CustomerControllerV2.java
@@ -1,6 +1,7 @@
 package com.pedrok.customerservice.customer;
 
 import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,13 +9,9 @@ import java.util.List;
 
 @RestController
 @RequestMapping("v2/customer")
+@AllArgsConstructor
 public class CustomerControllerV2 {
     private final CustomerService customerService;
-
-    @Autowired
-    public CustomerControllerV2(CustomerService customerService) {
-        this.customerService = customerService;
-    }
 
     @GetMapping
     public List<Customer> getCustomers() {
