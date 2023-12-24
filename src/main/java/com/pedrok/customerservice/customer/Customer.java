293b3d5355090755d436b59671a7dac1c8ae8ad0
pedrok <pedrokimur@icloud.com>
2023-12-24 14:46:09 -0300

    refactor(customer): remove boilerplate

--- src/main/java/com/pedrok/customerservice/customer/Customer.java
+++ src/main/java/com/pedrok/customerservice/customer/Customer.java
@@ -5,9 +5,15 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "customers")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,39 +29,4 @@ public class Customer {
     @NotNull(message = "email must not be empty")
     @Email(message = "email must be well-formatted")
     private String email;
-
-    public Customer(Long id, String name, String password, String email) {
-        this.id = id;
-        this.name = name;
-        this.password = password;
-        this.email = email;
-    }
-
-    public Customer() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-    @Override
-    public String toString() {
-        return "Customer{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", password='" + password + '\'' +
-                ", email='" + email + '\'' +
-                '}';
-    }
 }
--- src/main/java/com/pedrok/customerservice/customer/CustomerService.java
+++ src/main/java/com/pedrok/customerservice/customer/CustomerService.java
@@ -2,22 +2,17 @@ package com.pedrok.customerservice.customer;
 
 import com.pedrok.customerservice.exception.NotFoundException;
 import jakarta.transaction.Transactional;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@AllArgsConstructor
+@Slf4j
 public class CustomerService {
-    private final static Logger LOGGER = LoggerFactory.getLogger(CustomerService.class);
     private final CustomerRepository customerRepository;
-
-    @Autowired
-    public CustomerService(CustomerRepository customerRepository) {
-        this.customerRepository = customerRepository;
-    }
 
     public List<Customer> getCustomers() {
         return customerRepository.findAll();
@@ -27,7 +22,7 @@ public class CustomerService {
         return customerRepository.findById(id)
                 .orElseThrow(() -> {
                     NotFoundException notFoundException = new NotFoundException("customer with ID " + id + " not found");
-                    LOGGER.error("customer with ID {} not found", id, notFoundException);
+                    log.error("customer with ID {} not found", id, notFoundException);
                     return notFoundException;
                 });
     }
@@ -35,7 +30,7 @@ public class CustomerService {
     @Transactional
     public Customer createCustomer(Customer customer) {
         Customer customerCreated = customerRepository.save(customer);
-        LOGGER.info("{} created", customerCreated);
+        log.info("{} created", customerCreated);
         return customerCreated;
     }
 }
