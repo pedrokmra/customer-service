70a6a6c73f4282f14a90118b44db4b282ead4480
pedrok <pedrokimur@icloud.com>
2023-12-24 14:09:53 -0300

    refactor(exception): convert class to record

--- src/main/java/com/pedrok/customerservice/exception/ApiException.java
+++ src/main/java/com/pedrok/customerservice/exception/ApiException.java
@@ -6,46 +6,11 @@ import org.springframework.http.HttpStatus;
 import java.time.ZonedDateTime;
 import java.util.List;
 
-public class ApiException {
-    private final List<String> message;
-
-    @JsonIgnore
-    private final Throwable throwable;
-
-    private final HttpStatus httpStatus;
-
-    private final ZonedDateTime zonedDateTime;
-
-    public ApiException(List<String> message, Throwable throwable, HttpStatus httpStatus, ZonedDateTime zonedDateTime) {
-        this.message = message;
-        this.throwable = throwable;
-        this.httpStatus = httpStatus;
-        this.zonedDateTime = zonedDateTime;
-    }
-
-    public List<String> getMessage() {
-        return message;
-    }
-
-    public Throwable getThrowable() {
-        return throwable;
-    }
-
-    public HttpStatus getHttpStatus() {
-        return httpStatus;
-    }
-
-    public ZonedDateTime getZonedDateTime() {
-        return zonedDateTime;
-    }
-
-    @Override
-    public String toString() {
-        return "ApiException{" +
-                "message=" + message +
-                ", throwable=" + throwable +
-                ", httpStatus=" + httpStatus +
-                ", zonedDateTime=" + zonedDateTime +
-                '}';
-    }
+public record ApiException(
+        List<String> message,
+        @JsonIgnore
+        Throwable throwable,
+        HttpStatus httpStatus,
+        ZonedDateTime zonedDateTime
+) {
 }
