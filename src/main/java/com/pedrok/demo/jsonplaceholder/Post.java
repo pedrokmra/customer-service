baf2d1107f6698994dae332d679aa2b195188d33
pedrok <pedrokimur@icloud.com>
2023-12-24 10:37:08 -0300

    feat(jsonplaceholder): add REST client

---
+++ src/main/java/com/pedrok/demo/jsonplaceholder/Post.java
@@ -0,0 +1,41 @@
+package com.pedrok.demo.jsonplaceholder;
+
+public class Post {
+    private final Long userId;
+    private final Long id;
+    private final String title;
+    private final String body;
+
+    public Post(Long userId, Long id, String title, String body) {
+        this.userId = userId;
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "userId=" + userId +
+                ", id=" + id +
+                ", title='" + title + '\'' +
+                ", body='" + body + '\'' +
+                '}';
+    }
+}
