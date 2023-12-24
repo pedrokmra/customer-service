bd928194afadc2bb095a6ce1108a9a18bd05ce89
pedrok <pedrokimur@icloud.com>
2023-12-24 14:47:23 -0300

    refactor(infoapp): remove boilerplate

--- src/main/java/com/pedrok/customerservice/infoapp/InfoApp.java
+++ src/main/java/com/pedrok/customerservice/infoapp/InfoApp.java
@@ -1,5 +1,6 @@
 package com.pedrok.customerservice.infoapp;
 
+import lombok.Data;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -7,43 +8,11 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ConfigurationProperties(prefix = "info.app")
+@Data
 public class InfoApp {
     private String name;
     private String description;
     private String version;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getVersion() {
-        return version;
-    }
-
-    public void setVersion(String version) {
-        this.version = version;
-    }
-
-    @Override
-    public String toString() {
-        return "InfoApp{" +
-                "name='" + name + '\'' +
-                ", description='" + description + '\'' +
-                ", version='" + version + '\'' +
-                '}';
-    }
 
     @Bean
     CommandLineRunner runner(InfoApp infoApp) {
