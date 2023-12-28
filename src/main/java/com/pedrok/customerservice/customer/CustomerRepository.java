0807b30ad38b4da8dcbfc728e4ac1cdb628ef3b4
pedrok <pedrokimur@icloud.com>
2023-12-28 14:23:13 -0300

    add(customer): selectCustomerByPhoneNumber

--- src/main/java/com/pedrok/customerservice/customer/CustomerRepository.java
+++ src/main/java/com/pedrok/customerservice/customer/CustomerRepository.java
@@ -1,8 +1,19 @@
 package com.pedrok.customerservice.customer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    @Query(
+            value = "SELECT ID, NAME, PASSWORD, EMAIL, PHONE_NUMBER " +
+                    "  FROM CUSTOMERS C " +
+                    " WHERE C.PHONE_NUMBER = :phone_number",
+            nativeQuery = true
+    )
+    Optional<Customer> selectCustomerByPhoneNumber(@Param("phone_number") String phoneNumber);
 }
