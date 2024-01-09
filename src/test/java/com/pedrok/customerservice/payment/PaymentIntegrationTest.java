00bd75c8ad4c9d03c0d85b3c884540f98c9b175a
pedrok <pedrokimur@icloud.com>
2024-01-09 11:07:20 -0300

    chore: remove unused import

--- src/test/java/com/pedrok/customerservice/payment/PaymentIntegrationTest.java
+++ src/test/java/com/pedrok/customerservice/payment/PaymentIntegrationTest.java
@@ -16,7 +16,6 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Optional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
