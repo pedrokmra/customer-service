80a345fb8739dcddf2341ac9df989d75820cad00
pedrok <pedrokimur@icloud.com>
2024-01-12 14:03:19 -0300

    refactor(message): add TwilioApi

--- src/main/java/com/pedrok/customerservice/message/twilio/TwilioService.java
+++ src/main/java/com/pedrok/customerservice/message/twilio/TwilioService.java
@@ -1,42 +1,40 @@
 package com.pedrok.customerservice.message.twilio;
 
 import com.pedrok.customerservice.exception.NotSentException;
+import com.pedrok.customerservice.message.Message;
 import com.pedrok.customerservice.message.MessageSend;
 import com.pedrok.customerservice.message.MessageSender;
-import com.twilio.Twilio;
-import com.twilio.rest.api.v2010.account.Message;
-import com.twilio.type.PhoneNumber;
 
-
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Service
+@AllArgsConstructor
 @Slf4j
 @ConditionalOnProperty(
         value = "twilio.enabled",
         havingValue = "true"
 )
 public class TwilioService implements MessageSender {
-    public static final String ACCOUNT_SID = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
-    public static final String AUTH_TOKEN = "your_auth_token";
+    private final TwilioApi twilioApi;
+
     public static final String FROM = "5551999999";
 
     @Override
-    public MessageSend send(com.pedrok.customerservice.message.Message message) {
+    public MessageSend send(Message message) {
+        Map<String, String> params = new HashMap<>();
+        params.put("to", message.to());
+        params.put("from", FROM);
+        params.put("message", message.message());
+
         try {
-            Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
-
-            Message messageTwilio = Message.creator(
-                            new PhoneNumber(message.to()),
-                            new PhoneNumber(FROM),
-                            message.message()
-                    ).create();
-
-            log.info("twilioId: {} - message {} is send to {} ",
-                    messageTwilio.getSid(), message.message(), message.to());
-
+            twilioApi.send(params);
+            log.info("twilio - message {} is send to {} ", message.message(), message.to());
             return new MessageSend(true);
         } catch (Exception exception) {
             log.info("twilio - message {} is not send to {} ", message.message(), message.to());
